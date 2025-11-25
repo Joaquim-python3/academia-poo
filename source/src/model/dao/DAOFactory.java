8951cdedd6c4616a8c17372358d6a07528daccf3
Joaquim Cabral <joaquimcabral.2014@gmail.com>
2025-11-25 13:03:00 -0300

    cria funções factory das entidades

--- source/src/model/dao/DAOFactory.java
+++ source/src/model/dao/DAOFactory.java
@@ -3,10 +3,13 @@ package model.dao;
 import db.DB;
 import model.dao.impl.ClienteDAOJDBC;
 import model.dao.impl.MatriculaDAOJDBC;
+import model.dao.impl.TreinoDAOJDBC;
 
 public class DAOFactory {
     // criar um DAOfactory para cada entidade
     public static ClienteDAO criaClienteDAO(){
         return new ClienteDAOJDBC(DB.getConnection());
     }
+    public static MatriculaDAO criaMatriculaDAO(){return new MatriculaDAOJDBC(DB.getConnection());}
+    public static TreinoDAO criaTreinoDAO(){return new TreinoDAOJDBC(DB.getConnection());}
 }
